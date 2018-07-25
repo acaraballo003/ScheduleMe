4ff393172a7dd3e1dc3b494e71e33f48e82f48fb
acaraballo003 <caraballoar14@gmail.com>
2018-07-25 17:11:51 -0400

    trying to get users ade. Having issues with getting the body of the form to store it into the username field.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  User.find({}, 'username', function(err, users) {
+    if(err) console.error(err);
+
+    res.render('users/index', { users });
+  });
+});
+
+// New User
+router.get('/new', (req, res, next) => {
+  res.render('users/new');
+});
+
+// create a User
+router.post('/', (res, req, next) => {
+  const user = new User(req.body);
+
+  user.save(function(err, user) {
+    if (err) console.log(err);
+
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
