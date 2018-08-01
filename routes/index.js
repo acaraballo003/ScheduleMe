6d50c9ccbcff157b506720574f4af0e61fe1d0fa
acaraballo003 <caraballoar14@gmail.com>
2018-08-01 14:58:24 -0400

    Added a new style sheet and fiddled with the display

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const auth = require('./helpers/auth');
 
 
 /* GET Welcome page. */
@@ -32,7 +33,7 @@ router.post('/login', (req, res, next) => {
 });
 
 /* GET logout */
-router.get('/logout', (req, res, next) => {
+router.get('/logout', auth.requireLogin, (req, res, next) => {
   if (req.session) {
     req.session.destroy((err) => {
       if (err) return next(err);
